aed3c499755029415dddb093390cfea088338933
David Duke <ouroborot@gmail.com>
2013-04-25 10:30:56 -0700

    Decomposing works in all nodes containing stmts
    - Are there other exprs containing stmts besides Closure?
    - how will we instrument if ($q = something()) ?

--- src/PHPUnitScribe/NodeVisitor/Decomposer.php
+++ src/PHPUnitScribe/NodeVisitor/Decomposer.php
@@ -8,6 +8,8 @@ class PHPUnitScribe_NodeVisitor_Decomposer extends PHPParser_NodeVisitorAbstract
 
     protected $decomposing_queue = array();
 
+    protected $inner_stmts_decomposed = array();
+
     protected function pop_context()
     {
         return array_shift($this->context_stack);
@@ -48,7 +50,22 @@ class PHPUnitScribe_NodeVisitor_Decomposer extends PHPParser_NodeVisitorAbstract
     {
         return $node_name === 'PHPParser_Node_Stmt_Function' ||
             $node_name === 'PHPParser_Node_Stmt_ClassMethod' ||
-            $node_name === 'PHPParser_Node_Expr_Closure';
+            $node_name === 'PHPParser_Node_Expr_Closure' ||
+            $node_name === 'PHPParser_Node_Stmt_If' ||
+            $node_name === 'PHPParser_Node_Stmt_Case' ||
+            $node_name === 'PHPParser_Node_Stmt_Catch' ||
+            $node_name === 'PHPParser_Node_Stmt_Class' ||
+            $node_name === 'PHPParser_Node_Stmt_Declare' ||
+            $node_name === 'PHPParser_Node_Stmt_Do' ||
+            $node_name === 'PHPParser_Node_Stmt_Else' ||
+            $node_name === 'PHPParser_Node_Stmt_Elseif' ||
+            $node_name === 'PHPParser_Node_Stmt_For' ||
+            $node_name === 'PHPParser_Node_Stmt_Foreach' ||
+            $node_name === 'PHPParser_Node_Stmt_Interface' ||
+            $node_name === 'PHPParser_Node_Stmt_Namespace' ||
+            $node_name === 'PHPParser_Node_Stmt_Trait' ||
+            $node_name === 'PHPParser_Node_Stmt_TryCatch' ||
+            $node_name === 'PHPParser_Node_Stmt_While';
     }
 
     protected function in_nested_context()
@@ -57,15 +74,9 @@ class PHPUnitScribe_NodeVisitor_Decomposer extends PHPParser_NodeVisitorAbstract
         {
             return false;
         }
-        if (count($this->context_stack) == 1)
-        {
-            echo "context stack is 1\n";
-            var_dump($this->context_stack);
-        }
         if (count($this->context_stack) == 1 &&
             $this->context_stack[0] === 'PHPParser_Node_Expr_Assign')
         {
-            echo "not nested!\n";
             return false;
         }
 
@@ -80,7 +91,10 @@ class PHPUnitScribe_NodeVisitor_Decomposer extends PHPParser_NodeVisitorAbstract
             $inner_traverser = new PHPParser_NodeTraverser();
             $inner_decomposer = new PHPUnitScribe_NodeVisitor_Decomposer();
             $inner_traverser->addVisitor($inner_decomposer);
-            $node->stmts = $inner_traverser->traverse($inner_stmts);
+            //$node->stmts = $inner_traverser->traverse($inner_stmts);
+            $this->inner_stmts_decomposed = $inner_traverser->traverse($inner_stmts);
+            // Zero out the inner section so we don't do extra work
+            $node->stmts = array();
         }
     }
 
@@ -88,10 +102,14 @@ class PHPUnitScribe_NodeVisitor_Decomposer extends PHPParser_NodeVisitorAbstract
     {
         $this->pop_context();
         $var = null;
+        if (count($this->inner_stmts_decomposed) > 0 &&
+            is_array($inner_stmts = $this->get_statement_array($node)))
+        {
+            $node->stmts = $this->inner_stmts_decomposed;
+            $this->inner_stmts_decomposed = array();
+        }
         if (PHPUnitScribe_Interceptor::is_mockable_reference($node) &&
-            $this->in_nested_context() &&
-            count($this->context_stack) > 0  &&
-            $this->is_decomposition_enabled())
+            $this->in_nested_context())
         {
             $var_name = PHPUnitScribe_Interceptor::get_new_var_name();
             $var = new PHPParser_Node_Expr_Variable($var_name);
